5c38213c7c7668b8579af24fd549ff399ee8b7c6
KangWooJu <space1102@naver.com>
2026-05-07 16:58:36 +0900

    feat : authenticate jwt requests using redis auth cache

--- src/main/java/org/woojukang/springdefaultsetting/global/config/exception/BaseExceptionEnum.java
+++ src/main/java/org/woojukang/springdefaultsetting/global/config/exception/BaseExceptionEnum.java
@@ -12,7 +12,10 @@ public enum BaseExceptionEnum implements MessageCommInterface {
     ENTITY_NOT_FOUND("BASE.EXCEPTION.ENTITY_NOT_FOUND", "조회 대상이 없습니다."),
     FORBIDDEN("BASE.EXCEPTION.FORBIDDEN", "인증 실패 : {0}"),
     BAD_REQUEST("BASE.EXCEPTION.BAD_REQUEST", "잘못된 요청입니다."),
-    ACCESS_DENIED("BASE.EXCEPTION.ACCESS_DENIED", "{0}")
+    ACCESS_DENIED("BASE.EXCEPTION.ACCESS_DENIED", "{0}"),
+    JWT_EXPIRED("BASE.EXCEPTION.JWT_EXPIRED","JWT 토큰이 파기되었습니다."),
+    INVALID_TOKEN_CATEGORY("BASE.EXCEPTION.INVALID_TOKEN_CATEGORY","해당 토큰의 카테고리는 적절하지 않습니다."),
+    AUTH_SESSION_NOT_FOUND("BASE.EXCEPTION.AUTH_SESSION_NOT_FOUND","캐시에서 해당 유저를 찾을 수 없습니다.")
     ;
 
     private final String errorCode;
