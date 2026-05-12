bc8f66d79fb04d8009570bfc0c067eed18a11afc
KangWooJu <space1102@naver.com>
2026-05-12 16:29:04 +0900

    feat: configure JWT authentication filter chain

--- src/main/java/org/woojukang/springdefaultsetting/global/config/security/SecurityConfig.java
+++ src/main/java/org/woojukang/springdefaultsetting/global/config/security/SecurityConfig.java
@@ -1,8 +1,10 @@
 package org.woojukang.springdefaultsetting.global.config.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -12,11 +14,28 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.woojukang.springdefaultsetting.global.security.filter.JwtFilter;
+import org.woojukang.springdefaultsetting.global.security.filter.JwtLoginFilter;
+import org.woojukang.springdefaultsetting.global.security.filter.JwtLogoutFilter;
+import org.woojukang.springdefaultsetting.global.security.repository.UserAuthCacheRepository;
+import org.woojukang.springdefaultsetting.global.security.service.RefreshService;
+import org.woojukang.springdefaultsetting.global.security.util.JwtUtil;
+import org.woojukang.springdefaultsetting.global.security.validator.RefreshTokenValidator;
+import org.woojukang.springdefaultsetting.global.utils.web.CookieUtil;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final RefreshService refreshService;
+    private final CookieUtil cookieUtil;
+    private final RefreshTokenValidator refreshTokenValidator;
+    private final ObjectMapper objectMapper;
+    private final UserAuthCacheRepository userAuthCacheRepository;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
@@ -31,8 +50,59 @@ public class SecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    @Bean
+    public JwtLogoutFilter jwtLogoutFilter(){
+        return new JwtLogoutFilter(jwtUtil,
+                refreshService,
+                cookieUtil,
+                refreshTokenValidator);
+    }
+
+    @Bean
+    public JwtLoginFilter jwtLoginFilter(AuthenticationManager authenticationManager){
+        return new JwtLoginFilter(objectMapper,
+                authenticationManager,
+                jwtUtil,
+                refreshService,
+                cookieUtil);
+    }
+
+    @Bean
+    public JwtFilter jwtFilter(){
+        return new JwtFilter(jwtUtil,
+                userAuthCacheRepository);
+
+    }
+
+    // API 문서 및 옵저버 툴 관련 설정
+    @Bean
+    @Order(1)
+    public SecurityFilterChain apiSecurityFilterChain(HttpSecurity httpSecurity)
+            throws Exception {
+
+        httpSecurity
+                .securityMatcher(
+                        "/actuator/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/v3/api-docs/**",
+                        "/v3/api-docs",
+                        "/swagger-resources/**",
+                        "/webjars/**",
+                        "/mock/api/**"
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityFilterChain securityFilterChain
-            (HttpSecurity httpSecurity)
+            (HttpSecurity httpSecurity, JwtLoginFilter jwtLoginFilter)
             throws Exception{
 
         httpSecurity
@@ -40,9 +110,16 @@ public class SecurityConfig {
 
         httpSecurity
                 .formLogin(AbstractHttpConfigurer::disable);
-
         httpSecurity
                 .httpBasic(AbstractHttpConfigurer::disable);
+        httpSecurity
+                .addFilterAt(jwtLoginFilter,
+                        UsernamePasswordAuthenticationFilter.class); // 필터 순서2 (로그인)
+        httpSecurity
+                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class); // 필터 순서1 (JWT인증)
+        httpSecurity
+                .addFilterBefore(jwtLogoutFilter(), LogoutFilter.class); // 필터순서 1 ( 로그아웃 )
+
         httpSecurity
                 .cors(Customizer.withDefaults());
         httpSecurity
