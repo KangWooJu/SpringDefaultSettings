1b3db25a57989eb12a6d6603024f10d6d6158e65
KangWooJu <space1102@naver.com>
2026-08-07 13:59:23 +0900

    test : implements integration test template and add user domain integrationTest

---
+++ src/test/java/org/woojukang/springdefaultsetting/integration/domain/user/UserIntegrationTest.java
@@ -0,0 +1,6 @@
+package org.woojukang.springdefaultsetting.integration.domain.user;
+
+import org.woojukang.springdefaultsetting.integration.IntegrationTest;
+
+public class UserIntegrationTest extends IntegrationTest {
+}
