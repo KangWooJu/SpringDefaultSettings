54e3ec038318a7a23095a1497bee31aa5c5a54b1
KangWooJu <space1102@naver.com>
2026-08-07 13:58:29 +0900

    test : add unit test for refresh domain

---
+++ src/test/java/org/woojukang/springdefaultsetting/unit/global/security/facade/RefreshFacadeTest.java
@@ -0,0 +1,62 @@
+package org.woojukang.springdefaultsetting.unit.global.security.facade;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.woojukang.springdefaultsetting.global.security.dto.response.ReissueResponse;
+import org.woojukang.springdefaultsetting.global.security.facade.RefreshFacade;
+import org.woojukang.springdefaultsetting.global.security.service.RefreshService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class RefreshFacadeTest {
+
+    @Mock
+    private RefreshService refreshService;
+
+    @Mock
+    private HttpServletRequest request;
+
+    @InjectMocks
+    private RefreshFacade refreshFacade;
+
+    @Test
+    @DisplayName("토큰 재발급 성공시, ReissueResponse 반환")
+    void reissueSuccess() {
+
+        // given
+        ReissueResponse response = new ReissueResponse(
+                ReissueResponse
+                        .ReissueStatus
+                        .REFRESH_REISSUED,
+                "2026-07-20T15:00:00",
+                "new-access-token",
+                "new-refresh-token"
+        );
+
+        when(refreshService
+                .refreshCookies(request))
+                .thenReturn(response);
+
+        // when
+        ReissueResponse result = refreshFacade
+                .reissue(request);
+
+        // then
+
+        assertThat(result)
+                .isSameAs(response);
+
+        verify(refreshService)
+                .refreshCookies(request);
+    }
+}
